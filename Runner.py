52e1b1b1b3769ba10cf3df52a7b5cc597cf4b545
witmicko <mjogrodniczak@gmail.com>
2016-04-13 22:42:38 +0100

    runner fix

--- Runner.py
+++ Runner.py
@@ -7,21 +7,25 @@ choices = ('Camera Settings',
            'CAN bus settings',
            'Working Mode')
 
-choice = buttonbox(msg='Select mode:', title='VISE', choices=choices,
-                   image='gui/VISE_logo.png',
-                   default_choice=None,
-                   cancel_choice=None)
 
-if choice == 'Camera Settings':
-    os.system("CameraSettings.py")
+while True:
+    choice = buttonbox(msg='Select mode:', title='VISE', choices=choices,
+                       image='gui/VISE_logo.png',
+                       default_choice=None,
+                       cancel_choice=None)
 
-elif choice == 'Training Mode':
-    os.system("Training.py")
+    if choice is None:
+        break
+        
+    if choice == 'Camera Settings':
+        os.system("CameraSettings.py")
 
-elif choice == 'CAN bus settings':
-    os.system("CanBusSettings.py")
+    elif choice == 'Training Mode':
+        os.system("Training.py")
 
-elif choice == 'Working Mode':
-    os.system("ClusterReader.py")
+    elif choice == 'CAN bus settings':
+        os.system("CanBusSettings.py")
 
-print(choice)
+    elif choice == 'Working Mode':
+        os.system("ClusterReader.py")
+
